bae246f710759aeb40cfab648c12910d18c27b2a
ivan_lemeshkin <jablackalicious@gmail.com>
2023-07-15 14:49:58 +0300

    realize debug read-function data from test_db

--- src/main/kotlin/com/constructor/Application.kt
+++ src/main/kotlin/com/constructor/Application.kt
@@ -4,10 +4,6 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.constructor.plugins.*
-import database.models.example
-//import database.models.DatabaseFactory
-import freemarker.cache.ClassTemplateLoader
-import io.ktor.server.freemarker.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -16,8 +12,4 @@ fun main() {
 
 fun Application.module() {
     configureRouting()
-    example()
-    install(FreeMarker) {
-        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
-    }
 }
