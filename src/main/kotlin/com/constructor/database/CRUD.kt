4ca90e1e92b532695d512e2fb8f229daaf1c2aaf
ivan_lemeshkin <jablackalicious@gmail.com>
2023-07-17 23:28:16 +0300

    realize debug read-function data from test_db + re-writting the function of reading a record from db + re-writting the function of deleting a record by ID + re-writting function of creating a record in db table

--- src/main/kotlin/com/constructor/database/CRUD.kt
+++ src/main/kotlin/com/constructor/database/CRUD.kt
@@ -1,8 +1,7 @@
 package com.constructor.database
 
 import java.io.Closeable
-import java.sql.DriverManager
-import java.sql.Statement
+import java.sql.*
 
 class Connection(private val coreConnection: java.sql.Connection) :
     java.sql.Connection by coreConnection, Closeable {
@@ -15,84 +14,76 @@ class Connection(private val coreConnection: java.sql.Connection) :
         return coreConnection.createStatement()
     }
 }
+
+fun connect(): Connection {
+    val url = "jdbc:postgresql://localhost:5432/Test"
+    val user = "postgres"
+    val password = "tyX7~Lp3+"
+
+    val c = DriverManager.getConnection(url, user, password)
+    c.autoCommit = false
+    return Connection(c)
+}
+
 class CRUD
 {
+    // надо решить подобное говно
+    val url = "jdbc:postgresql://localhost:5432/Test"
+    val user = "postgres"
+    val password = "tyX7~Lp3+"
+
+    val c = DriverManager.getConnection(url, user, password)
+    val temp = c
+    //mb out temp. how it in kotlin?..
+
     fun create(id: Int, stringLine : String, num : Int, boolField : Boolean)
-    { //
-        connect().use {
-            val st1 = it.createStatement()
-            val resultSet1 = st1.executeQuery(
-                """
-            INSERT INTO test_table VALUES ($id.ToString() , $stringLine , $num.toString() , $boolField.toString())
-            """.trimIndent()
-            )
-            resultSet1.close()
-            st1.close()
+    {
+        //done!
+
+        try {
+            val resultSet1 = """
+        INSERT INTO test_table
+        (id, stringfield, intfield, boolfield) VALUES (?, ?, ?, ?)
+        """
+
+            val preparedStatement: PreparedStatement = temp.prepareStatement(resultSet1)
+            preparedStatement.setInt(1, id)
+            preparedStatement.setString(2, stringLine)
+            preparedStatement.setInt(3, num)
+            preparedStatement.setBoolean(4, boolField)
+
+            val rows = preparedStatement.executeUpdate()
+            println("entry add: $rows")
+        }
+        catch (e : Exception)
+        {
+            println("Entry not added")
         }
     }
 
-    fun read() : ArrayList<String>{
-        connect().use {
-            val st1 = it.createStatement()
-            val resultSet1 = st1.executeQuery(
-                """
-            SELECT *
-              FROM test_table
-             ORDER BY id
-            """.trimIndent()
-            )
+    fun read() : MutableList<String>
+    {
+        val result : MutableList<String> = mutableListOf()
 
-            val DataToReturn = arrayListOf<String>()
+       val querySql = """SELECT *
+               FROM test_table
+               ORDER BY id
+        """
+       val createStatement : Statement = temp.createStatement()
+       val data = createStatement.executeQuery(querySql)
 
-            // не универсальный код. Решение только для одной таблицы
-            while (resultSet1.next()) {
-                val a = resultSet1.getString("id")
-                val b = resultSet1.getString("stringfield")
-                val c = resultSet1.getString("intfield")
-                val d = resultSet1.getString("boolfield")
-
-                // плохо знаю пока что синтаксис
-                // можно было и через цикл
-                DataToReturn.add(a)
-                DataToReturn.add(b)
-                DataToReturn.add(c)
-                DataToReturn.add(d)
-            }
-            resultSet1.close()
-            st1.close()
-            return DataToReturn
-        }
+       while (data.next())
+       {
+            // mb Map?
+       }
+       return result
     }
     fun delete(id : Int)
     {
-        val i_id = id.toString()
-
-        connect().use {
-            val st1 = it.createStatement()
-            try {
-                val resultSet1 = st1.executeQuery(
-                    """
-                DELETE FROM test_table 
-                WHERE id = $i_id ;
-                """.trimIndent()
-                ) // запрос некоректно составлен
-                resultSet1.close()
-                st1.close()
-            }
-            catch (e : Exception)
-            {
-                println("Dont work")
-                println(e.message)
-            }
-        }
+        // done
+        val preparedStatement : PreparedStatement = temp.prepareStatement("DELETE FROM test_table WHERE Id = ?")
+        preparedStatement.setInt(1, id)
+        preparedStatement.executeUpdate()
     }
-    fun connect(): Connection {
-        val url = "jdbc:postgresql://localhost:5432/Test"
-        val user = "postgres"
-        val password = "tyX7~Lp3+"
 
-        val c = DriverManager.getConnection(url, user, password)
-        c.autoCommit = false
-        return Connection(c)
-    }
 }
