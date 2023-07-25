29ef1e96b77a8cddafe3262c1ecb835f1f7a52b5
ivan_lemeshkin <jablackalicious@gmail.com>
2023-07-25 18:45:53 +0300

    done crud

--- src/main/kotlin/com/constructor/Application.kt
+++ src/main/kotlin/com/constructor/Application.kt
@@ -8,9 +8,9 @@ import org.jetbrains.exposed.sql.Database
 
 val db_conn = Database.connect(
     "jdbc:postgresql://localhost:5432/Test", driver = "org.postgresql.Driver",
-    user = "postgres", password = "tyX7~Lp3+"
+    user = "postgreSql_dude", password = "password"
 )
-//
+
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
--- src/main/kotlin/com/constructor/serializationJson.kt
+++ src/main/kotlin/com/constructor/serializationJson.kt
@@ -3,7 +3,7 @@ package com.constructor
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
-//
+
 fun Application.configureSerialization()
 {
     install(ContentNegotiation)
