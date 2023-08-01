4dfa114e4b4f264e919325a3bb3b348fe0cabd47
Filipp Semenkov <filipp.semenkov@lightspeedhq.com>
2023-08-02 01:20:36 +0400

    make multimodule

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,17 @@
-val ktor_version: String by project // todo: Property name 'ktor_version' should not contain underscores
-val kotlin_version: String by project // todo: Property name 'ktor_version' should not contain underscores
-val logback_version: String by project // todo: Property name 'ktor_version' should not contain underscores
-val exposedVersion: String by project
-val postgresqlVersion: String by project
-
 plugins {
-  kotlin("jvm") version "1.9.0"
-  id("io.ktor.plugin") version "2.3.2"
-  kotlin("plugin.serialization") version "1.9.0"
+  kotlin("multiplatform") version "1.9.0" apply false
+  kotlin("plugin.serialization") version "1.9.0" apply false
 }
 
-group = "com.constructor"
-version = "0.0.1"
-application {
-  mainClass.set("com.constructor.ApplicationKt")
-
-  val isDevelopment: Boolean = project.ext.has("development")
-  applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+tasks.wrapper {
+  distributionType = Wrapper.DistributionType.ALL
+  gradleVersion = "8.2.1"
 }
 
-repositories {
-  mavenCentral()
-}
-// library
-dependencies {
-  implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-  implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-  implementation("ch.qos.logback:logback-classic:$logback_version")
-  testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-  testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-  // exposed lib - for db CRUD and OOP stuff
-  implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-  implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-  implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-  // html
-  //implementation("io.ktor:ktor-server-freemarker:$ktor_version")
-  implementation("io.ktor:ktor-server-html-builder:$ktor_version")
-  implementation("io.ktor:ktor-client-core:$ktor_version")
-  implementation("io.ktor:ktor-client-cio:$ktor_version")
-  implementation("org.postgresql:postgresql:$postgresqlVersion")
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jre7:1.2.71") // todo: move version to gradle.properties
-  // json
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1") // todo: move version to gradle.properties
-  // uuid
-  implementation("com.eaio.uuid:uuid:3.2") // todo: move version to gradle.properties
+allprojects {
+  version = "0.1.1" // todo: generate by git version
+
+  repositories {
+    mavenCentral()
+  }
 }
---
+++ libs/db/src/main/kotlin/com/appconstructor/libs/db/DbConfig.kt
@@ -0,0 +1,5 @@
+package com.appconstructor.libs.db
+
+data class DbConfig(
+  val dbName: String,
+)
