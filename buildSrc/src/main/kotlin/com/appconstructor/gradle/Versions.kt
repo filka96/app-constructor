b2a934596668faa52ccc5092bcda6559109b5eb1
Filipp Semenkov <filipp.semenkov@lightspeedhq.com>
2023-08-28 11:08:07 +0300

    update library version

--- buildSrc/src/main/kotlin/com/appconstructor/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/com/appconstructor/gradle/Versions.kt
@@ -1,14 +1,14 @@
 package com.appconstructor.gradle
 
 object Versions {
-  const val kotlin = "1.9.0"
-  const val ktor = "2.3.2"
-  const val exposed = "0.42.0"
+  const val kotlin = "1.9.10"
+  const val ktor = "2.3.3"
+  const val exposed = "0.42.1"
   const val postgresql = "42.6.0"
   const val h2 = "2.2.220"
 
   const val kotlinxDatetime = "0.4.0"
-  const val kotlinxSerializationJson = "1.5.1"
+  const val kotlinxSerializationJson = "1.6.0"
 
-  const val logback = "1.4.8"
+  const val logback = "1.4.11"
 }
